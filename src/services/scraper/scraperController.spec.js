a4324e2dd044952962cc86f54ab7cacf37c04082
MatheusAltino <matheusaltino.dev@gmail.com>
2022-04-30 22:41:05 -0300

    feature/config jest

---
+++ src/services/scraper/scraperController.spec.js
@@ -0,0 +1,15 @@
+const { default: axios } = require('axios')
+const { data } = require('cheerio/lib/api/attributes')
+const getNotbooks = require('./scraperController')
+
+describe('Scraper Tests', () => {
+    it('Data has load', () => {
+
+    expect()     
+    })
+    
+    it('', () => {
+    
+    expect()
+    })
+})
